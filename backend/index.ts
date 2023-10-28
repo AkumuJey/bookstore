e6e1ee9178e8280120d1099340800457b74f9496
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-10-28 23:18:19 +0300

    Coonected to the database successfully

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,31 @@
-import express, {Express, Response, Request} from 'express'
+import express, {Express} from 'express'
+import { connectToDb, getDb } from './db'
 import router from './routes'
 
-
+//Initialization of the app and middlewares
 const app: Express = express()
-
 app.use(express.json())
+
+
 app.use('/', router)
 
 const PORT = 5000
+//Connecting to db
 
-app.listen(PORT, () => {
-    console.log(`App is running at ${PORT}`);
+let db
+connectToDb((err : Error) => {
+    if (!err) {
+        app.listen(PORT, () => {
+            console.log(`App is running at ${PORT}`);
+        })
+        db = getDb()
+    } else {
+        console.log('Failed o connect');
+        
+    }
 })
+
+
+
+
+
