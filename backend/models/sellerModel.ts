f246962ccf0805ae95c3558447d7ceeb26530860
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-11-04 23:27:30 +0300

    Seeting up route for acount creation

--- backend/models/sellerModel.ts
+++ backend/models/sellerModel.ts
@@ -1,33 +1,43 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose";
 
+export interface SellerType extends Document {
+  name: string;
+  email: string;
+  password: string;
+  dateOfBirth: Date;
+  retailStats: {
+    totalPurchases: number;
+  };
+}
 const sellerSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        dateOfBirth: {
-            type: Date,
-            required: true
-        },
-        retailStats: {
-            totalPurchases: {
-                type: Number,
-                required: true
-            },
-        }
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    retailStats: {
+      totalPurchases: {
+        type: Number,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Seller = mongoose.model('Seller', sellerSchema)
+export const Seller = mongoose.model("Seller", sellerSchema);
+
--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,13 +1,32 @@
 import express from 'express'
+import { Seller, SellerType } from '../models/sellerModel'
+
 
 const signupRoute = express.Router()
 const loginRoute = express.Router()
 
-signupRoute.post('/', (req, res) => {
-    res.send('Hellow world')
+signupRoute.post('/', async (req, res) => {
+    try {
+        const seller = await Seller.findOne({email: req.body.email})
+        if (seller) {
+            res.status(409).json({
+                message: "Seller already exists"
+            })
+        }
+        if(!seller) {
+            res.status(201).json({
+                message: "Account created"
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to store client"
+        })
+    }
 })
 loginRoute.post('/', (req, res) => {
     res.send('Hellow world Login')
+    
 })
 
 export {
