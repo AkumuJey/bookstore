89e5de3c7946db7bb6f2f9184b5c692b3b908e62
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-02-21 14:19:00 +0300

    Added password validation to the backend

--- backend/src/db/users.ts
+++ backend/src/db/users.ts
@@ -14,7 +14,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
     length: 8,
-    select: false,
+    
   },
   role: {
     type: String,
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { genSalt, hash } from "bcrypt";
+import { compare, genSalt, hash } from "bcrypt";
 import { UserModel } from "../db/users";
 
 const signupRoute = express.Router();
@@ -24,7 +24,8 @@ signupRoute.post("/", async (req, res) => {
             username,
             role,
           });
-          newUser.save()
+          newUser
+            .save()
             .then((data) => {
               res.status(201).json(data);
             })
@@ -38,8 +39,30 @@ signupRoute.post("/", async (req, res) => {
   }
 });
 loginRoute.post("/", async (req, res) => {
-  const users = await UserModel.find();
-  res.send(users);
+
+  try {
+    await UserModel.findOne({email: req.body.email}).then(async  (user)=> {
+      if (!user) {
+        res.status(401).json({message: "Login failed"})
+        return
+      }
+      await compare(req.body.password +  process.env.SECRET_PASSWORD, user.password, (err, result) =>{
+        if (err) {
+          res.status(401).json({ err})
+        }
+        if(!result){
+          res.status(401).json({ message:"Wrong Password!"})
+          return
+        }else{
+          res.status(200).json({message: "Suceess", user, result});
+        }
+      });
+      
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error });
+  }
 });
 
 export { loginRoute, signupRoute };
