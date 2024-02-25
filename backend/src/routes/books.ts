01289207307b2ae5677d8a57d71700c263d0aa6c
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-02-25 23:35:09 +0300

    Added route to get data for single book

--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -43,7 +43,7 @@ booksRoute.delete("/:id", async (req, res) => {
         res.status(400).json({status:"Invalid ID"})
         return
     }
-    let result = await BookModel.findByIdAndRemove(id);
+    const result = await BookModel.findByIdAndRemove(id);
     res.status(200).json({ status: "Deleted", result });
   } catch (error) {
     res.status(500).json({
@@ -52,5 +52,25 @@ booksRoute.delete("/:id", async (req, res) => {
     });
   }
 });
+
+booksRoute.get("/:id", async(req, res) => {
+    try {
+        const id = req.params.id;
+        if (!id) {
+            res.status(400).json({status:"Invalid ID"})
+            return
+        }
+        const book = await BookModel.findById(id)
+        res.status(200).json({
+          status:'Success',
+          data:book
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: "Error",
+            message: error.message,
+          });
+    }
+})
 
 export default booksRoute;
