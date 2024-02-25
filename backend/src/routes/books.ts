58d96d466f314ddb6eaaead8771ad808303fee14
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-02-25 23:30:36 +0300

    Added route to delete book

--- backend/src/routes/books.ts
+++ backend/src/routes/books.ts
@@ -9,7 +9,7 @@ booksRoute.get("/", async (req, res) => {
     res.status(201).json({
       status: "Success",
       data: books,
-      length: books.length
+      length: books.length,
     });
   } catch (error) {
     res.status(401).json({
@@ -20,20 +20,37 @@ booksRoute.get("/", async (req, res) => {
 });
 
 booksRoute.post("", async (req, res) => {
-    try {
-        const bookData = req.body
-        const  newBook = new BookModel(bookData);
-        await newBook.save()
-        res.status(201).json({
-            status:"success",
-            data:newBook
-        })
-    } catch (error) {
-        res.status(401).json({
-            status:"Faild",
-            message : error.message
-        })
+  try {
+    const bookData = req.body;
+    const newBook = new BookModel(bookData);
+    await newBook.save();
+    res.status(201).json({
+      status: "success",
+      data: newBook,
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: "Faild",
+      message: error.message,
+    });
+  }
+});
+
+booksRoute.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+        res.status(400).json({status:"Invalid ID"})
+        return
     }
-})
+    let result = await BookModel.findByIdAndRemove(id);
+    res.status(200).json({ status: "Deleted", result });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+});
 
 export default booksRoute;
