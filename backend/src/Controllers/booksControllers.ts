cf040561e5eec62ed6e15c3fa23f244f783bf11d
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2024-05-07 09:37:19 +0300

    Refactored books controllers

--- backend/src/Controllers/booksControllers.ts
+++ backend/src/Controllers/booksControllers.ts
@@ -16,3 +16,78 @@ export const getBooksController = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const addBookController = async (req: Request, res: Response) => {
+  try {
+    const bookData = req.body;
+    const newBook = new BookModel(bookData);
+    await newBook.save();
+    res.status(201).json({
+      status: "success",
+      data: newBook,
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
+
+export const deleteBookController = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      res.status(400).json({ status: "Invalid ID" });
+      return;
+    }
+    const result = await BookModel.findByIdAndRemove(id);
+    res.status(200).json({ status: "Deleted", result });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+};
+
+export const findBookController = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    if (!id) {
+      res.status(400).json({ status: "Invalid ID" });
+      return;
+    }
+    const book = await BookModel.findById(id);
+    res.status(200).json({
+      status: "Success",
+      data: book,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const editBookController = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+    if (!id) {
+      return res.status(500).json({ status: "Invalid ID" });
+    }
+    const updateBook = await BookModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(201).json({
+      status: "Updated",
+      data: updateBook,
+      body,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- backend/src/routes/booksRoute.ts
+++ backend/src/routes/booksRoute.ts
@@ -1,84 +1,22 @@
-import { getBooksController } from "../Controllers/booksControllers";
-import { BookModel } from "../Models/bookModel";
+import {
+  addBookController,
+  deleteBookController,
+  editBookController,
+  findBookController,
+  getBooksController,
+} from "../Controllers/booksControllers";
 import express from "express";
 
 const booksRoute = express.Router();
 
 booksRoute.get("/", getBooksController);
 
-booksRoute.post("", async (req, res) => {
-  try {
-    const bookData = req.body;
-    const newBook = new BookModel(bookData);
-    await newBook.save();
-    res.status(201).json({
-      status: "success",
-      data: newBook,
-    });
-  } catch (error) {
-    res.status(401).json({
-      status: "Failed",
-      message: error.message,
-    });
-  }
-});
+booksRoute.post("", addBookController);
 
-booksRoute.delete("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    if (!id) {
-      res.status(400).json({ status: "Invalid ID" });
-      return;
-    }
-    const result = await BookModel.findByIdAndRemove(id);
-    res.status(200).json({ status: "Deleted", result });
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message,
-    });
-  }
-});
+booksRoute.delete("/:id", deleteBookController);
 
-booksRoute.get("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    if (!id) {
-      res.status(400).json({ status: "Invalid ID" });
-      return;
-    }
-    const book = await BookModel.findById(id);
-    res.status(200).json({
-      status: "Success",
-      data: book,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
+booksRoute.get("/:id", findBookController);
 
-booksRoute.patch("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const body = req.body;
-    if (!id) {
-      return res.status(500).json({ status: "Invalid ID" });
-    }
-    const updateBook = await BookModel.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    res.status(201).json({
-      status: "Updated",
-      data: updateBook,
-      body,
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
+booksRoute.patch("/:id", editBookController);
 
 export default booksRoute;
